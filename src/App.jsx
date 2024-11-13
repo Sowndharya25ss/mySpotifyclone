69fda9293fac405e345c20a11f456f64ced9975d
Sowndharya25ss <sowndharya.ca25@gmail.com>
2024-11-13 22:24:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
+import React, { useState } from "react";
 import TopContainer from "./Components/TopContainer";
-import MiddleContainer from "./Components/MiddleContainer";
 import BottomContainer from "./Components/BottomContainer";
+import SideBarContainer from "./Components/SideBarContainer";
+import FeaturedCharts from "./Components/FeaturedCharts";
+import PopularArtists from "./Components/PopularArtists";
+import PopularAlbums from "./Components/PopularAlbums";
+import BottomLinks from "./Components/BottomLinks";
+import Copyright from "./Components/Copyright";
 
 import "./App.css";
-import "./Components/mainContainers.css"
-import "./Components/cards.css"
+import "./Components/mainContainers.css";
+import "./Components/cards.css";
 
 function App() {
-
+  const [home, setHome] = useState(true);
+  const [login, setLogin] = useState(false)
   return (
     <>
-      <TopContainer/>
-      <MiddleContainer/>
-      <BottomContainer/>
+      {login ? (
+        <div>
+          <h1>ppp</h1>
+        </div>
+      ) : (
+        <div>
+          <TopContainer />
+
+          <div className="middle-container">
+            {/* Side Bar Container */}
+            <SideBarContainer />
+
+            {/* Main Bar Container */}
+            <div className="main-container">
+              <PopularArtists />
+              <PopularAlbums />
+              <FeaturedCharts />
+              <BottomLinks />
+              <hr></hr>
+              <Copyright />
+            </div>
+          </div>
+
+          <BottomContainer />
+        </div>
+      )}
     </>
   );
 }
---
+++ src/Components/LoginOrSignup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginOrSignup() {
+  return <div>LoginOrSignup</div>;
+}
+
+export default LoginOrSignup;
